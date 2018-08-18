a12818af12201c9f4fe7a7ba5b8602009be164de
Ronan <ronan.16@hotmail.com>
2018-08-17 21:45:58 -0300

    Classes Usuario Programa Criada

--- src/aplicacao/Programa.java
+++ src/aplicacao/Programa.java
@@ -1,9 +1,17 @@
 package aplicacao;
 
+import dominio.Usuario;
+
 public class Programa {
 
 	public static void main(String[] args) {
-		System.out.println("TESTE");
+		Usuario u = new Usuario(1, "ROnan", "asodao@asodoasd.com");
+		Usuario u2 = new Usuario(2, "ROnan2", "asodao@asodoasd.com");
+		Usuario u3 = new Usuario(3, "ROnan3", "asodao@asodoasd.com");
+		
+		System.out.println(u.toString());
+		System.out.println(u2.toString());
+		System.out.println(u3.toString());
 	}
 
 }
--- src/dominio/Usuario.java
+++ src/dominio/Usuario.java
@@ -1,5 +1,49 @@
 package dominio;
 
-public class Usuario {
+import java.io.Serializable;
 
+public class Usuario implements Serializable{
+	
+	private static final long serialVersionUID = 1L;
+	Integer id;
+	String nome;
+	String senha;
+	
+	public Usuario(Integer id, String nome, String senha) {
+		super();
+		this.id = id;
+		this.nome = nome;
+		this.senha = senha;
+	}
+
+	public Integer getId() {
+		return id;
+	}
+
+	public void setId(Integer id) {
+		this.id = id;
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public String getSenha() {
+		return senha;
+	}
+
+	public void setSenha(String senha) {
+		this.senha = senha;
+	}
+
+	@Override
+	public String toString() {
+		return "Usuario [id=" + id + ", nome=" + nome + ", senha=" + senha + "]";
+	}
+	
+	
 }
